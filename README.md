6d8bb9d3316cada781dcecd9fe3b33920eab18e3
Vee-v <46638059+Vee-v@users.noreply.github.com>
2022-12-30 03:21:49 -0300

    Using median filter to detect only deepest lines, only works in one order. Shift is calculated with the centroid offset of the deepest line. readme fixed.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Dependencies
 Tested with Python 3.10.8
+
 numpy
+
 scipy
+
 astropy
+
 specutils>=1.8.0 (via pip)
--- spextractor.py
+++ spextractor.py
@@ -1,5 +1,7 @@
 from astropy.io import fits
 from astropy import units as u
+from astropy import uncertainty as unc
+from astropy.modeling import models
 from pathlib import Path
 import numpy as np
 import scipy
@@ -7,6 +9,10 @@ import warnings
 import argparse
 import specutils
 from specutils import Spectrum1D
+from specutils.spectra import SpectralRegion
+from specutils.manipulation import noise_region_uncertainty, extract_region, gaussian_smooth, trapezoid_smooth
+from specutils.fitting import find_lines_threshold, estimate_line_parameters, fit_lines
+
 
 # GLOBAL VARIABLES
 current_dir = Path.cwd()
@@ -20,55 +26,86 @@ def read_fits(folder='TIC220414682'):
     The function needs a string with the name of the target in TIC 
     format as an input (default=TIC220414682).
     """
-
+    
     folder = current_dir / 'targets' / folder
     fitsList = [x for x in folder.iterdir() if not (x.is_dir())]
     spectra = []
-    headers = []
     if len(fitsList) < 2:
         print(f'Target {folder} has only 1 spectrum. Skipping...\n')
-        return spectra, headers
+        return spectra
     for fileName in fitsList:
         with fits.open(fileName) as hdul:
-            hdr = hdul[0].header
             data = hdul[0].data
             spectra.append(data)
-            headers.append(hdr)
-    return np.array(spectra), headers
+    return np.array(spectra)
 
 
-def zeroing(spectra, headers):
+def shifting(spectra):
     """Function which receives a read_fits output tuple elements and
-    returns the normalized spectrum for each order at RV = 0 and each
+    returns the normalized spectrum for each order at RV = refRV and each
     fits file.
     """
-
-    # First we create the arrays which will hold the zeroed spectrum
-    # information:
+    # We cycle through all the fits image
+    reference_pos = 0.
+    shifts = np.zeros(spectra.shape[0])
+    with warnings.catch_warnings():  # Ignore warnings
+        warnings.simplefilter('ignore')
+        for specNum in range(spectra.shape[0]):
+            linesList = []
+            spectraList = []
+            wls_aux = np.empty(spectra[specNum, 0, :].shape)
+            flux_aux = np.empty(spectra[specNum, 5, :].shape)
+            mask = np.ones(spectra[specNum, 0, :].shape)
+            for order in range(spectra.shape[2]):
+                wls_aux[order] = spectra[specNum, 0, order]
+                flux_aux[order] = spectra[specNum, 5, order]
+                # We can use the wls_aux instead of wls cuz snr is better at longer wl
+                central_wavelength = np.median(wls_aux[order])  
+                # Se encuentra los rangos espectrales superpuestos entre ordenes contiguos.
+                if order > 0:
+                    mask = wls_aux[order] < shortestWl
+                    wls, flux = wls_aux[order][mask], flux_aux[order][mask]
+                    spectrum = Spectrum1D(flux=(flux-np.median(flux))*u.adu, spectral_axis=wls*u.AA)
+                else:
+                    spectrum = Spectrum1D(flux=(flux_aux[order]-np.median(flux_aux[order]))*u.adu,
+                                          spectral_axis=wls_aux[order]*u.AA)    
+                spectrum = gaussian_smooth(spectrum, stddev=8)
+                spectrum = noise_region_uncertainty(spectrum, SpectralRegion(5825*u.AA, 5850*u.AA))
+                if not(np.any(np.isnan(spectrum.uncertainty.array))):
+                    
+                    lines = find_lines_threshold(spectrum, noise_factor=3)
+                    linesList.append(lines[lines['line_type'] == 'absorption'])
+                    spectraList.append(spectrum)
+                shortestWl = wls_aux[order, 0]
+            linesList = np.array(linesList)
+            spectraList = np.array(spectraList)
+            deep = 0  # depth of deepest line
+            pos = 0   # wavelength of deepest line
+            pos2 = 0  # wavelength of the 2nd deepest line
+            for order in range(linesList.shape[0]):
+                for i in linesList[order]['line_center_index']:
+                    line_region = SpectralRegion(spectraList[order].spectral_axis[i-4], spectraList[order].spectral_axis[i+4])
+                    line_spectrum = extract_region(spectraList[order], line_region)
+                    estimation = estimate_line_parameters(line_spectrum, models.Gaussian1D())
+                    if estimation.amplitude < deep:
+                        deep = estimation.amplitude
+                        pos2 = pos
+                        pos = estimation.mean
+            # The first spectrum is the reference spectrum, so we need to save the reference position.
+            if specNum == 0:
+                reference_pos = pos.value
+            else:
+                shifts[specNum] = pos.value - reference_pos
+    # First we create the arrays which will hold the zeroed spectrum information
     fullSpectrum = np.zeros((spectra.shape[0], spectra.shape[2]), dtype=object)
     signal2Noise = np.zeros((spectra.shape[0], spectra.shape[2], 2048))
-    # Then we cycle through all the fits image data and header.
-    for fitsNum in range(spectra.shape[0]):
-        # We allocate the RV, Flux array and Wavelength array for each fits.
-        rv = headers[fitsNum]['RV'] * u.Unit("km/s")
-        flux = spectra[fitsNum][5] * u.adu
-        wl = spectra[fitsNum][0] * u.AA
-        snr = spectra[fitsNum][8]  # We don't need to shift the SNR because the operations are index-based.
-        # We create the 1D spectrum object from specutils package for each order
+    # Now we cycle through all the fits images again and we shift them and save them.
+    for specNum in range(spectra.shape[0]):
         for order in range(spectra.shape[2]):
-            spectrum = Spectrum1D(
-                spectral_axis=wl[order],
-                flux=flux[order],
-                radial_velocity=rv
-            )
-            # Now we shift the spectrum to RV=0.
-            try:
-                spectrum.shift_spectrum_to(radial_velocity=0 * u.Unit("km/s"))
-            except:
-                raise Exception(
-                    f'This function needs specutils version >=1.8.0. Your version is {specutils.__version__}.')
-            fullSpectrum[fitsNum][order] = spectrum
-            signal2Noise[fitsNum, order, :] = snr[order]
+            shifted_order = Spectrum1D(spectral_axis=spectra[specNum, 0, order]*u.AA + shifts[specNum]*u.AA,
+                                       flux=spectra[specNum, 5, order]*u.adu)
+            fullSpectrum[specNum, order] = shifted_order
+            signal2Noise[specNum, order] = spectra[specNum, 8, order]
     return fullSpectrum, signal2Noise
 
 
@@ -141,10 +178,10 @@ def main(targetList=None):
     with open(path, 'r') as f:
         lines = f.readlines()
         for target in lines:
-            spectra, header = read_fits(folder=target.strip())
+            spectra= read_fits(folder=target.strip())
             if len(spectra) < 2:
                 continue
-            fullSpectrum, signal2Noise = zeroing(spectra, header)
+            fullSpectrum, signal2Noise = shifting(spectra)
             coadded = coadding(fullSpectrum, signal2Noise, target.strip())
             print(f'Target {target.strip()} has been coadded with {fullSpectrum.shape[0]} spectra.')
             data_to_ceres(coadded, name=target.strip())
